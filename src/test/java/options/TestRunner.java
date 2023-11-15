5b53c30dec3c15e9028b9495cd4f50dac5b64ffc
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-15 20:55:20 +0100

    Creating TestRunner class

---
+++ src/test/java/options/TestRunner.java
@@ -0,0 +1,11 @@
+package options;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources", plugin ="json:target/jsonReports/cucumber-report.json",
+        glue = "stepDefinitions")
+public class TestRunner {
+}
