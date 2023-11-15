25262f74ef36da438641dbcf6f947477eda6dc9b
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-15 20:59:01 +0100

    Creating enum Api Resources

---
+++ src/test/java/resources/ApiResources.java
@@ -0,0 +1,19 @@
+package resources;
+
+public enum ApiResources {
+
+    AddPlaceAPI("/maps/api/place/add/json"),
+    getPlaceAPI("/maps/api/place/get/json"),
+    deletePlaceAPI("/maps/api/place/delete/json");
+    private String resource;
+
+    ApiResources(String resource)
+    {
+        this.resource=resource;
+    }
+
+    public String getResource()
+    {
+        return resource;
+    }
+}
